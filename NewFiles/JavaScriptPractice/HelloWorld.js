f25e29eb5af5487075aa7764c0205479c72d8505
Cesenor <Senorce@outlook.com>
2020-07-10 22:09:15 -0500

    update

--- NewFiles/JavaScriptPractice/HelloWorld.js
+++ NewFiles/JavaScriptPractice/HelloWorld.js
@@ -9,3 +9,10 @@ array[0] = "alex";
 array[1] = true;
 array[2] = 53;
 console.log(array[2]);
+
+var array2 = ("dog", true, 45); // with parathensis  ( )
+var array3 = ["tree", 2, 4]; //with brackets [ ]
+
+for(i = 0; i<6; i++){
+    console.log("Iteration " + i + "<br>");
+}
